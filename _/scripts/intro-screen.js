d671bfa28a7a72630e606184c2740ef1e70fdcda
Quinton Aiken <quintonlaiken@gmail.com>
2015-06-10 20:13:13 -0500

    add module pattern to intro-screen js

--- _/scripts/intro-screen.js
+++ _/scripts/intro-screen.js
@@ -2,18 +2,37 @@ var cssClasses = require('./css-classes');
 
 module.exports = (function() {
 
-  var mainTitle = document.querySelector(cssClasses.title);
-  var form = document.querySelector(cssClasses.form);
-  var projectile = document.querySelector(cssClasses.projectile);
+  var mainTitle;
+  var form;
+  var projectile;
+  var clicked = false;
 
-  mainTitle.addEventListener('click',function(e) {
-    
-    this.classList.add('fade-out');
+  var titleClick = function() {
 
-    form.classList.add('fade-in');
-    projectile.classList.add('fade-in');
-  });
+    mainTitle.addEventListener('click',function(e) {
+      clicked = true;
+      
+      this.classList.add('fade-out');
 
-  return true;
+      form.classList.add('fade-in');
+      projectile.classList.add('fade-in');
+    });
+
+  };
+
+  var init = function() {
+    mainTitle = document.querySelector(cssClasses.title);
+    form = document.querySelector(cssClasses.form);
+    projectile = document.querySelector(cssClasses.projectile);
+
+    titleClick();
+  };
+
+  return {
+    init: init,
+    clicked: function() {
+      return clicked;
+    }
+  };
 
 }());
